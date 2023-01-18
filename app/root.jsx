1619471d59856183ee1321a4c005a23de66b0793
Max Hargreave <max@monsun.dk>
2023-01-18 15:58:05 +0100

    css working, not in components

--- app/root.jsx
+++ app/root.jsx
@@ -7,25 +7,38 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import GlobalNav from "./shared/components/GlobalNav";
+import styles from "./styles/global.css";
+
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
 });
+
+export const links = () => {
+    return [{ rel: "stylesheet", href: styles}];
+};
 
 export default function App() {
   return (
     <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+        <head>
+            <Meta />
+            <Links />
+        </head>
+        <body>
+            <header>
+                <GlobalNav />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer></footer>
+            <ScrollRestoration />
+            <Scripts />
+            <LiveReload />
+        </body>
     </html>
   );
 }
---
+++ app/shared/components/GlobalNav/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "@remix-run/react";
+
+export default function GlobalNav() {
+    return ( 
+        <nav className="global-nav">
+            <Link to="/">Home</Link>{" "}
+            <Link to="/about" prefetch="intent">About</Link>{" "}
+            <Link to="/contact" prefetch="intent">Contact</Link>
+        </nav>
+    );
+}
+ 
